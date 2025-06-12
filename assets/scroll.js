3bbc7609b496148f983fe98e4634377dfe6a61be
jal <jal@hayloft-it.ch>
2025-06-12 13:43:14 +0200

    added scroll animations

---
+++ assets/scroll.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(async (entries, observer) => {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                await new Promise(r => setTimeout(r, 400));
+                entry.target.classList.add('show');
+            }
+        }
+    });
+
+    const hiddenElements = document.querySelectorAll('.scroll');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    window.addEventListener(
+        "scroll",
+        () => {
+            document.body.style.setProperty(
+                "--scroll",
+                window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+            );
+        },
+        false
+    );
+});
